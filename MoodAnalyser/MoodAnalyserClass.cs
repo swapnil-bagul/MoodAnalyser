0f81833969f325297101dc0d497cb35089eb951f
swapnil-bagul <bagulswapnilp@gmail.com>
2021-11-05 18:53:48 +0530

    [Swapnil] Added UC1 Ability to analyse and respond

--- MoodAnalyser/MoodAnalyserClass.cs
+++ MoodAnalyser/MoodAnalyserClass.cs
@@ -4,7 +4,23 @@ using System.Text;
 
 namespace MoodAnalyser
 {
-    class MoodAnalyserClass
+    public class MoodAnalyserClass
     {
+        public string message;
+
+        public MoodAnalyserClass(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyzeMood(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "SAD";
+            }
+            else
+                return "HAPPY";
+        }
+
     }
 }
--- MoodAnalyserTest/MoodAnalyseTestClass.cs
+++ MoodAnalyserTest/MoodAnalyseTestClass.cs
@@ -9,8 +9,22 @@ namespace MoodAnalyserTest
     public class MoodAnalyseTestClass
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AnalyseMoodHappyOrSad()
         {
+            // Test Case 1
+            //Arrange
+            string message = "I am in sad mood";
+           // string message = "I am in happy mood";
+
+            string expected = "SAD";
+
+            //Act
+            MoodAnalyserClass moodAnalyserClass  = new MoodAnalyserClass(message );
+            string actual = moodAnalyserClass.AnalyzeMood(message);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
         }
     }
 }
