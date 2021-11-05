42f1c4ac63dbf10b2b02d4adacd6ddd727b4f43f
swapnil-bagul <bagulswapnilp@gmail.com>
2021-11-05 17:48:26 +0530

    [Swapnil] Added Mood Analyser Code

---
+++ MoodAnalyserTest/MoodAnalyseTestClass.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using MoodAnalyser;
+
+
+namespace MoodAnalyserTest
+{
+    [TestClass]
+    public class MoodAnalyseTestClass
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
